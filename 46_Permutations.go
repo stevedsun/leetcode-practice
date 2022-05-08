c4876d330e3d33d72979d707a2098d713f85911b
Steve Sun <>
2022-05-08 22:26:31 +0800

    update

---
+++ 46_Permutations.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func permute(nums []int) [][]int {
+	var result = [][]int{}
+	if len(nums) == 1 {
+		result = append(result, []int{nums[0]})
+		return result
+	}
+	for i := 0; i < len(nums); i++ {
+		temp := make([]int, len(nums), len(nums))
+		copy(temp, nums)
+		temp[i] = temp[len(temp)-1]
+		temp = temp[:len(temp)-1]
+		fmt.Println("nums=", nums, "temp=", temp)
+		for _, res := range permute(temp) {
+			result = append(result, append([]int{nums[i]}, res...))
+		}
+	}
+
+	return result
+}
+
+func main() {
+	fmt.Println(permute([]int{1, 2, 3}))
+}
