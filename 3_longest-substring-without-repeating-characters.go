0b416bfe21e394928601ff9cb23a2fe90ee6133b
Steve Sun <>
2022-05-08 22:29:06 +0800

    update

---
+++ 3_longest-substring-without-repeating-characters.go
@@ -0,0 +1,38 @@
+package main
+
+import "strings"
+
+func lengthOfLongestSubstring(s string) int {
+	if s == "" {
+		return 0
+	}
+	var res int = 1
+	var i int = 0
+	var j int = 0
+	for {
+		if i >= len(s) || j >= len(s) {
+			break
+		}
+		if i >= j {
+			j = j + 1
+			continue
+		}
+		if strings.Contains(s[i:j], string(s[j])) {
+			if len(s[i:j]) > res {
+				res = len(s[i:j])
+			}
+			i = i + 1
+		} else {
+			if len(s[i:j+1]) > res {
+				res = len(s[i : j+1])
+			}
+			j = j + 1
+		}
+	}
+
+	return res
+}
+
+func main() {
+	lengthOfLongestSubstring("aabbcc")
+}
